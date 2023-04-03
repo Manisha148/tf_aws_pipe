0f167dd2d590eaecd4d96b0f206ae60f12498d59
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-04-03 13:41:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@
 
 # Configure the AWS provider
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 # Create a VPC with CIDR block 10.0.0.0/16
@@ -99,20 +99,20 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   route_table_id = aws_route_table.my_rt.id
 }
 
-# Create an EC2 instance in the public subnet
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
+# # Create an EC2 instance in the public subnet
+# resource "aws_instance" "my_ec2" {
+#   ami           = "ami-0c55b159cbfafe1f0"
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.public_subnet.id
 
-  tags = {
-    Name = "my_ec2"
-  }
-}
+#   tags = {
+#     Name = "my_ec2"
+#   }
+# }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-bucket-name"
+  bucket = "09876yhkhgbb"
 
   tags = {
     Name = "my_bucket"
